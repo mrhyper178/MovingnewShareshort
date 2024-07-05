d8348d06fc40556b4adcefbac1035af309bd348c
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-07-05 09:34:18 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -39,20 +39,20 @@ class Config(object):
   #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002119561803"))
   #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002003476986")
 
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_Series_Bot")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002035253761"))
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002003476986")
+  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "QL_Series_Bot")
+  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002035253761"))
+  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002003476986")
 
   #BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   #BOT_USERNAME = os.environ.get("BOT_USERNAME", "Vijay_Tv_SerialsBot")
   #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002117688872"))
   #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
 
-  #BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  #BOT_USERNAME = os.environ.get("BOT_USERNAME", "SunTvShowsBot")
-  #LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002001724354"))
-  #UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "SunTvShowsBot")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002001724354"))
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001728131839")
 
   #BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   #BOT_USERNAME = os.environ.get("BOT_USERNAME", "AdultOnly18_Bot")
