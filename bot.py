2b2ad6923d380519dc040cd70e10b60cdbd70d89
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-08-26 20:14:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -379,7 +379,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                     )
                     return
             except UserNotParticipant:
-                invite_link = await get_invite_link(channel_chat_id)
+                invite_link = await get_invite_link(bot, channel_chat_id)
                 await cmd.message.edit(
                     text="**I like Your Smartness But Don't Be Oversmart! 😑**\n\n",
                     reply_markup=InlineKeyboardMarkup(
