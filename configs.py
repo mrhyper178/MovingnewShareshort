111cfbb08b8bb2afde8a407e0e85585bdd0aaac1
mrhyper178 <153428922+mrhyper178@users.noreply.github.com>
2024-08-31 07:27:31 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config(object):
   API_HASH = os.environ.get("API_HASH", "f19aed00b0c74abed0359016afc1733f")
   
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Tamilan_Rocks8_Bot")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Tamilan_Rocks9_Bot")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001884285982"))
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001859384286")
 
